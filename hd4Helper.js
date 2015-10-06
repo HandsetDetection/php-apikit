10c1e39029441568f4b23488ab8b92d4d615ce5c
Richard Uren <richard@teleport.com.au>
2015-10-06 15:04:00 +1100

    Added js helper for taking hardware samples to improve detection results when differeing devices have teh same http headers. BUGFIX : Fixed a bug in HDDevice/getHighAccuracyCandidates

---
+++ hd4Helper.js
@@ -0,0 +1,28 @@
+(function () {
+	function serialize( obj ) {
+	  return '?'+Object.keys(obj).reduce(function(a,k){a.push(k+'='+encodeURIComponent(obj[k]));return a},[]).join('&');
+	}
+	var b = {},bd = [],bmt =+ new Date() +50;
+	b.w = window.screen.width || 0;
+	b.h = window.screen.height || 0;
+	b.r = ~~((window.devicePixelRatio || 1) * 100);
+
+	do {
+		var t,count=0,tt =+ new Date();
+		do { t =+ new Date(); } while (t === tt)
+		do { count++; tt= +new Date(); } while (t === tt)
+		if ((tt-t) === 1) { bd[bd.length] = count; }
+	} while ((bd.length < 20) && (t < bmt))
+
+	var sum = 0, n = bd.length || 1;
+	while (bd.length) { sum += bd.pop(); }
+	b.m = ~~sum/n;
+	var d = new Date(); d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+	var str = serialize(b);
+	document.cookie = ("hd41=" + str + ";path=/;expires=" + d.toGMTString());
+
+	//var di = document.createElement('script');
+	//di.async = true;
+	//di.src = '//api.handsetdetection.com/apiv4/js/22/2s.js' + str;
+	//document.getElementsByTagName('head')[0].appendChild(di);
+}());
---
+++ testHDDevice.php
@@ -0,0 +1,66 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+require_once('HD4.php');
+require_once('HDDevice.php');
+require_once('HDStore.php');
+
+// The device class performs the same functions as our Cloud API, but locally.
+// It is only used when use_local is set to true in the config file.
+// To perform tests we need to setup the environment by populating the the Storage layer with device specs.
+// So install the latest community edition so there is something to work with.
+
+class HDDeviceTest extends PHPUnit_Framework_TestCase {
+
+	// Setup community edition for tests. Takes 60s or so to download and install.
+	static function setUpBeforeClass() {
+		$dir = sys_get_temp_dir();
+		$file = $dir . DIRECTORY_SEPARATOR . "communityTest.zip";
+		$hd4 = new HandsetDetection\HD4();
+
+		$store = HandsetDetection\HDStore::getInstance();
+		$store->setPath($dir, true);
+
+		$hd4->remote("community/fetcharchive", '', 'zip', false);
+		$status = file_put_contents($dir . DIRECTORY_SEPARATOR . "communityTest.zip", $hd4->getRawReply());
+
+		$zip = new \ZipArchive();
+		if ($zip->open($file) === false)
+			return false;
+
+		for ($i = 0; $i < $zip->numFiles; $i++) {
+			$filename = $zip->getNameIndex($i);
+			$zip->extractTo($dir, $filename);
+			$store->moveIn($dir . DIRECTORY_SEPARATOR . $filename, $filename);
+		}
+		$zip->close();
+		return true;
+	}
+
+	// Remove community edition
+	static function tearDownAfterClass() {
+		$store = HandsetDetection\HDStore::getInstance();
+		$store->purge();
+	}
+
+	function testIsHelperRequiredTrue() {
+		$headers = array(
+			'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3 like Mac OS X; en-gb) AppleWebKit/533.17.9 (KHTML, like Gecko)'
+		);
+
+		$hdDevice = new HandsetDetection\HDDevice();
+		$result = $hdDevice->isHelperUseful($headers);
+		$this->assertTrue($result);
+	}
+
+	function testIsHelperRequiredFalse() {
+		$headers = array(
+			'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+		);
+
+		$hdDevice = new HandsetDetection\HDDevice();
+		$result = $hdDevice->isHelperUseful($headers);
+		$this->assertFalse($result);
+	}
+}
+
